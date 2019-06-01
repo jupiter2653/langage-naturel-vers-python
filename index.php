0ec936305b0eaea92bf7e793196af773ac36887d
Jupiter653 <keyvanattarian@gmail.com>
2019-06-02 00:17:35 +0200

    Ajout des plus et modification de l'id des input/output

--- index.php
+++ index.php
@@ -4,31 +4,31 @@
 	<title>Traducteur de langage naturel vers Python</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap74.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<!--Code mirror syntax highlighter-->
 	<link rel="stylesheet" href="codemirror-5.47.0/lib/codemirror.css">
 </head>
-<body class="container">
+<body>
 	<header>
 		<h1>Traducteur de langage naturel vers Python</h1>
 	</header>
-	<section class="row">
-		<div id="input">
+	<section>
+		<div id="inputArea">
 			<h2>Langage Naturel</h2>
 			<div id="inputForm" class="form-control">
-				<div class="element">
+				<div id="var" class="element">
 					<h3>Variables :</h3>
 					<div id="var1" class="line">
-						<input type="text" name="variables" class="oneChar form-control ">
+						<input type="text" name="variables" class="oneChar form-control">
 						<span>est un(e)</span>
-						<select class="form-control" id="var1Type">
+						<select class="form-control">
 							<option value="int">Entier</option>
 							<option value="float">Flottant</option>
 							<option value="string">Chaine de charactère</option>
 						</select>
 					</div>
+					<button onclick="addLine('var')" class="button-control line">+</button>
 				</div>
-				<div class="element">
+				<div id="input" class="element">
 					<h3>Entrée :</h3>
 					<div id="input1" class="line">
 						<span>Demander </span>
@@ -38,8 +38,9 @@
 							<option value="C">C</option>
 						</select>
 					</div>
+					<button onclick="addLine('input')" class="button-control line">+</button>
 				</div>
-				<div class="element">
+				<div class="element" id="process">
 					<h3>Traitement :</h3>
 					<div id="process1" class="line">
 						<select class="form-control" id="process1Var">
@@ -50,8 +51,9 @@
 						<span>Prend la valeur</span>
 						<input type="text" name="calculus" class="form-control">
 					</div>
+					<button onclick="addLine('process')" class="button-control line">+</button>
 				</div>
-				<div class="element lastElement">
+				<div class="element lastElement" id="output">
 					<h3>Sortie :</h3>
 					<div id="output1" class="line">
 						<span>Afficher </span>
@@ -61,13 +63,14 @@
 							<option value="C">C</option>
 						</select>
 					</div>
+					<button onclick="addLine('output')" class="button-control line">+</button>
 				</div>
 			</div>
 		</div>
-		<div id="output">
+		<div id="outputArea">
 			<h2>Python</h2>
-			<textarea class="form-control" readonly id="outputArea">def coucou(lel):
-	return True
+			<textarea class="form-control" readonly id="outputTextArea">
+a=int(input("> "))
 </textarea>
 		</div>
 	</section>
--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 //Creating code mirror instance
-var textArea = document.getElementById("outputArea");
+var textArea = document.getElementById("outputTextArea");
 var myCodeMirror = CodeMirror.fromTextArea(textArea,{mode : "text/x-python",readOnly : true});
+
+
+function addLine(type){
+	nbrLine = $("#"+type+" .line").length-1
+	console.log(nbrLine)
+	$("#"+type + nbrLine).clone().insertAfter("#"+type + nbrLine);
+	newNbrLine = $("#"+type+" .line").length-1
+	$("#"+type + nbrLine).attr("id",type + newNbrLine)
+}
