542625a19fabadb49bab2aa40aea4c95e075b7fb
Jupiter653 <keyvanattarian@gmail.com>
2019-06-09 23:40:07 +0200

    Sync vars lists with var given in 'variable'

--- scripts.js
+++ scripts.js
@@ -4,22 +4,31 @@ var myCodeMirror = CodeMirror.fromTextArea(textArea,{mode : "text/x-python",read
 
 
 function addLine(type){
-	nbrLine = $("#"+type+" .line").length-1
+	var nbrLine = $("#"+type+" .line").length-1
 	$("#"+type + nbrLine+" .remove-button").remove();
 	$("#"+type + nbrLine).clone().insertAfter("#"+type + nbrLine);
-	newNbrLine = $("#"+type+" .line").length-1
+	var newNbrLine = $("#"+type+" .line").length-1
 	$("#"+type + nbrLine).attr("id",type + newNbrLine)
 	$("#"+type + newNbrLine).append('<button onclick="delLastLine(\''+type+'\')" class="button-control remove-button">&times;</button>')
-	
+	$( "input" ).change()
 }
 function delLastLine(type){
-	nbrLine = $("#"+type+" .line").length-1
+	var nbrLine = $("#"+type+" .line").length-1
 	if (nbrLine > 2) { //S'il y a plus de 2 ligne on supprime la dernière et on déplace la crois
 		$("#"+type + nbrLine).remove();
-		newNbrLine = $("#"+type+" .line").length-1
+		var newNbrLine = $("#"+type+" .line").length-1
 		$("#"+type + newNbrLine).append('<button onclick="delLastLine(\''+type+'\')" class="button-control remove-button">&times;</button>')
 	}
 	else if(nbrLine  == 2){ //Si il y en a 2 on supprime juste la dernière
 		$("#"+type + nbrLine).remove();
+	}
+}
+function setNewVars() {
+	var nbrLine = $("#var .line").length-1
+	var tempName
+	$("#input .var-list option, #process .var-list option, #output .var-list option").remove()
+	for(i=nbrLine;i>=1;i-=1){
+		tempName = $("#var"+i+" input").val()
+		$("#input .var-list, #process .var-list, #output .var-list").append('<option value="'+tempName+'">'+tempName+'</option>')
 	}
 }
