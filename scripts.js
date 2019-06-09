802416c7e2319e95d0638bfca0b42d853799dbbf
Jupiter653 <keyvanattarian@gmail.com>
2019-06-09 20:44:07 +0200

    Added cross to delete a line of the input

--- scripts.js
+++ scripts.js
@@ -5,8 +5,16 @@ var myCodeMirror = CodeMirror.fromTextArea(textArea,{mode : "text/x-python",read
 
 function addLine(type){
 	nbrLine = $("#"+type+" .line").length-1
-	console.log(nbrLine)
+	$("#"+type + nbrLine+" .remove-button").remove();
 	$("#"+type + nbrLine).clone().insertAfter("#"+type + nbrLine);
 	newNbrLine = $("#"+type+" .line").length-1
 	$("#"+type + nbrLine).attr("id",type + newNbrLine)
+	$("#"+type + newNbrLine).append('<button onclick="delLastLine(\''+type+'\')" class="button-control remove-button">&times;</button>')
+	
+}
+function delLastLine(type){
+	nbrLine = $("#"+type+" .line").length-1
+	$("#"+type + nbrLine).remove();
+	newNbrLine = $("#"+type+" .line").length-1
+	$("#"+type + newNbrLine).append('<button onclick="delLastLine(\''+type+'\')" class="button-control remove-button">&times;</button>')
 }
