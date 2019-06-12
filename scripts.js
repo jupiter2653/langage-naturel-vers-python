09a738c8061e5ed3f40474ebe61a4789555551af
Jupiter653 <keyvanattarian@gmail.com>
2019-06-12 20:35:00 +0200

    Add process and output translation

--- scripts.js
+++ scripts.js
@@ -27,6 +27,10 @@ function delLastLine(type){
 			setNewVars()
 		case 'input':
 			setInput()
+		case 'process':
+			setValues()
+		case 'output':
+			setOutput()
 	}
 }
 document["nameList"] = []
@@ -35,7 +39,7 @@ function setNewVars() {
 	var tempName
 	$("#input .var-list option, #process .var-list option, #output .var-list option").remove()
 	document["nameList"] = []
-	for(i=nbrLine;i>=1;i-=1){
+	for(var i=nbrLine;i>=1;i-=1){
 		tempName = $("#var"+i+" input").val()
 		tempType = $("#var"+i+" select").val()
 		document["nameList"].push([tempName,tempType])
@@ -43,23 +47,48 @@ function setNewVars() {
 			'<option value="'+tempName+'">'+tempName+'</option>')
 	}
 	setInput()
+    setValues()
+    setOutput()
 }
 document['inputList'] = []
 function setInput(){
 	var nbrLine = $("#input .line").length-1
 	var tempIn
 	document['inputList'] = []
-	for(i=nbrLine;i>=1;i-=1){
+	for(var i=nbrLine;i>=1;i-=1){
 		tempIn = $("#input"+i+" select").val()
 		document['inputList'].push(tempIn)
 	}
 	writeCode()
 }
+document['processingList'] = []
+function setValues(){
+	var nbrLine = $("#process .line").length-1
+	var tempVal
+	document['processingList'] = []
+	for (var i=nbrLine;i>=1;i-=1) {
+		var tempVal = $("#process"+i+" input").val()
+		var tempVar = $("#process"+i+" select").val()
+		document['processingList'].push([tempVar,tempVal])
+	}
+	writeCode()
+}
+document['outputList'] = []
+function setOutput(){
+	var nbrLine = $("#output .line").length-1
+	var tempVal
+	document['outputList'] = []
+	for (var i=nbrLine;i>=1;i-=1) {
+		var tempVar = $("#output"+i+" select").val()
+		document['outputList'].push(tempVar)
+	}
+	writeCode()
+}
 function writeCode(){
 	var nbrVar = document["nameList"].length
-	CodeMirrorInstance.setValue("")
-	for(i=0;i < nbrVar;i+=1){
-		if (document['inputList'].includes(document["nameList"][i][0])) {//is element in array
+	CodeMirrorInstance.setValue("") //We clean the output
+	for(var i=0;i < nbrVar;i+=1){ //For each variable
+		if (document['inputList'].includes(document["nameList"][i][0])) {//if element is input list
 			CodeMirrorInstance.setValue(
 				CodeMirrorInstance.getValue() + document["nameList"][i][0] + " = "+ document["nameList"][i][1] + "(input('> ')) \n")
 		}
@@ -68,4 +97,17 @@ function writeCode(){
 				CodeMirrorInstance.getValue() + document["nameList"][i][0] + " = "+ document["nameList"][i][1] + "() \n")
 		}
 	}
+	for (var i = 0; i < document['processingList'].length; i++) { //Foreach process element
+		CodeMirrorInstance.setValue(
+				CodeMirrorInstance.getValue() + document['processingList'][i][0] + " = "+ document['processingList'][i][1] + " \n")	
+	}
+	for (var i = 0; i < document['outputList'].length; i++) { //foreach output element
+		CodeMirrorInstance.setValue(
+				CodeMirrorInstance.getValue() + "print("+ document['outputList'][i][0] + ") \n")	
+	}
 }
+$(function() {
+    setNewVars()
+    setValues()
+    setOutput()
+});
